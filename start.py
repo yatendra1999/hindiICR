7140f6bc2b98a8f247ce723f49cfda1cdc3b87e6
Yatendra singh <yatendra1999luffy@gmail.com>
2018-08-27 19:28:44 +0530

    uci dataset
    added uci dataset

--- start.py
+++ start.py
@@ -6,21 +6,27 @@ from keras.preprocessing.image import array_to_img, img_to_array, load_img
 print(keras.__version__)
 model = keras.Sequential()
 # first layer
-model.add(keras.layers.Dense(128, activation='relu', input_shape=(36,36,3)))
+model.add(keras.layers.Dense(128, activation='relu', input_shape=(32,32,3)))
 model.add(keras.layers.Flatten())
 #second layers
 model.add(keras.layers.Dense(64, activation='relu'))
 #softmax layers
-model.add(keras.layers.Dense(12, activation='softmax'))
+model.add(keras.layers.Dense(46, activation='softmax'))
 model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='categorical_crossentropy',metrics=[keras.metrics.categorical_accuracy])
 
 batch_size = 16
 test_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 train_generator = test_datagen.flow_from_directory(
-        'vowels',
-        target_size=(36, 36),
-        batch_size=14,
+        'uci/train',
+        target_size=(32, 32),
+        batch_size=20,
         class_mode='categorical')
+#
+# validation_generator = test_datagen.flow_from_directory(
+#         'vowels',
+#         target_size=(36, 36),
+#         batch_size=14,
+#         class_mode='categorical')
 
 import os.path
 if os.path.exists('model.h5'):
@@ -29,7 +35,7 @@ if os.path.exists('model.h5'):
 
 model.fit_generator(
         train_generator,
-        steps_per_epoch=460 // batch_size,
+        steps_per_epoch=3910 // batch_size,
         epochs=50,
         verbose=1,
         shuffle=True)
