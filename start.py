3e24950484626ef3c159e81265ff899ce276110f
Yatendra singh <yatendra1999luffy@gmail.com>
2018-08-27 14:05:11 +0530

    load weights

--- start.py
+++ start.py
@@ -8,9 +8,9 @@ model = keras.Sequential()
 # first layer
 model.add(keras.layers.Dense(128, activation='relu', input_shape=(36,36,3)))
 model.add(keras.layers.Flatten())
-#second layer
+#second layers
 model.add(keras.layers.Dense(64, activation='relu'))
-#softmax layer
+#softmax layers
 model.add(keras.layers.Dense(12, activation='softmax'))
 model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='categorical_crossentropy',metrics=[keras.metrics.categorical_accuracy])
 
@@ -22,10 +22,15 @@ train_generator = test_datagen.flow_from_directory(
         batch_size=14,
         class_mode='categorical')
 
+import os.path
+if os.path.exists('model.h5'):
+    model.load_weights('model.h5')
+
+
 model.fit_generator(
         train_generator,
         steps_per_epoch=460 // batch_size,
         epochs=50,
         verbose=1,
         shuffle=True)
-model.save_weights('first_try.h5')
+model.save_weights('model.h5')
